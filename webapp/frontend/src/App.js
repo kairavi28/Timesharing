b0c1b8f3d9edc18867437dd258a205eca724fe43
kairavi28 <kairavi.parmar2014@gmail.com>
2021-06-11 03:01:58 +0530

    Initial commit for Login, registration and template design

---
+++ webapp/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/header';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Temp from './components/template';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     <Temp/>
+    </div>
+  );
+}
+
+export default App;
