78807f69995fd67da92c6b49ec91fc1c6b5390f5
kairavi28 <kairavi.parmar2014@gmail.com>
2021-06-11 18:33:02 +0530

    Init design commit

--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import Header from './components/header';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import Temp from './components/template';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import Home from './components/homepage';
+import Sample from './components/sample';
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-     <Temp/>
-    
-     </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route  exact path='/sample' component={Sample}/>
+        {/* <Route  exact path='/2' component={Page2}/>
+        <Route  exact path='/3' component={Page3}/> */}
+      </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
