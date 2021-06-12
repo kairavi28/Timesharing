4f786d8b3be7772b154e041c0439a41b55b59271
owen byomuhangi <owen.byomuhangi@leveltenenergy>
2021-06-12 15:01:08 +0300

    Add bidding page

--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -1,17 +1,23 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Home from './components/homepage';
 import Sample from './components/sample';
+import Bidding from './components/bidding';
+import BaseNav from "./components/nav"
 
 function App() {
   return (
+    <>
+    <BaseNav />
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route  exact path='/sample' component={Sample}/>
+        <Route  exact path='/bidding' component={Bidding}/>
         {/* <Route  exact path='/2' component={Page2}/>
         <Route  exact path='/3' component={Page3}/> */}
       </Switch>
     </BrowserRouter>
+    </>
   );
 }
 
--- webapp/frontend/src/components/homepage.js
+++ webapp/frontend/src/components/homepage.js
@@ -3,27 +3,15 @@ import { Navbar, Nav, Form, FormControl, Button, Container, Row, Col } from 'rea
 import Carousal from './carousal';
 import Sample from './sample';
 
+
 function Homepage() {
     const bg_sm = {backgroundColor: "lightgrey", opacity: '0.8'};
     return(
         <>
-          <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Timesharing</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#wallet">Wallet</Nav.Link>
-              <Nav.Link href="#bidding">Bidding</Nav.Link>
-              <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-              <Nav.Link href="#profile">Profile</Nav.Link>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-info">Search</Button>
-            </Form>
-          </Navbar>
           <Container fluid style={bg_sm}>
               <Row>
               <Container>
-              <Carousal/>  
+              <Carousal/> 
               </Container> 
               </Row>
           </Container>
